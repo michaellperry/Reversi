0490f7bb92736e80adeebad597628fc45c5ebcd4
mperry <mperry@ddab523f-d738-40ac-a495-303371edec7e>
2011-02-16 05:20:04 +0000

    Increased font size of touchable text. Changed URL to actual domain.
    git-svn-id: http://fwutil01.facetedworlds.com/svn/clientapps/Reversi/trunk@314 ddab523f-d738-40ac-a495-303371edec7e

--- FacetedWorlds.Reversi/Presenters/POXConfigurationProvider.cs
+++ FacetedWorlds.Reversi/Presenters/POXConfigurationProvider.cs
@@ -25,7 +25,7 @@ namespace FacetedWorlds.Reversi.Presenters
         {
             get
             {
-                return new POXConfiguration("http://184.106.132.210/correspondence_server_web/pox", "FacetedWorlds.Reversi");
+                return new POXConfiguration("http://facetedworlds.com/correspondence_server_web/pox", "FacetedWorlds.Reversi");
                 //return new POXConfiguration("http://michael-pc:8080/correspondence_server_web/pox", "FacetedWorlds.Reversi");
             }
         }
